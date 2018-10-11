5a64ed7c6db9937b496b695af338bbe5df2bf0e9
Andrés Miguel Terán <amt@MacBook-Pro-de-Andres.local>
2018-10-12 01:06:22 +0200

    Added base config

---
+++ server.js
@@ -0,0 +1,12 @@
+var { graphql, buildSchema } = require("graphql");
+
+var schema = buildSchema(`
+    type Query {
+        hello: String
+    }`);
+
+var root = {
+  hello: () => "Hello World"
+};
+
+graphql(schema, "{ hello }", root).then(res => console.log("RESPONSE", res));
